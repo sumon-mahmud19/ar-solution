36648411595705e090aeba31ced4fe47618ff043
sclsumonislam@gmail.com <sclsumonislam@gmail.com>
2025-05-26 08:32:58 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ar Easy Solution</title>
+</head>
+<body>
+    <h1 class="text-indigo-700 font-extrabold text-7xl justify-center text-center">
+        Welcome, Ar Easy Solution!
+    </h1>
+</body>
+</html>
