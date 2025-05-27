de2a5e58879292ed6a7205a7586bcf8100ddb597
sclsumonislam@gmail.com <sclsumonislam@gmail.com>
2025-05-27 07:49:08 +0600

    design home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,8 @@
     
     @yield('content')
 
-    @include('components.navbar')
-    
+    @include('components.footer')
+
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 
