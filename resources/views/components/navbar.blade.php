681565aeb0a1c519f84e20979852950e667614b0
sclsumonislam@gmail.com <sclsumonislam@gmail.com>
2025-05-27 07:37:39 +0600

    design home page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,8 +3,8 @@
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
         <a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+             
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ar-Solution</span>
         </a>
         <div class="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
             <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
